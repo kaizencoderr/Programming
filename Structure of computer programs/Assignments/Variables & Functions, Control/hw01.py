ff6c0bac0049b78ed95262211fcd803b3aaca336
kaizencoderr <93795896+kaizencoderr@users.noreply.github.com>
2022-01-22 16:03:20 -0800

    Add files via upload

---
+++ Structure of computer programs/Assignments/Variables & Functions, Control/hw01.py
@@ -0,0 +1,265 @@
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+from operator import add, sub
+
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> # a check that you didn't change the return statement!
+    >>> import inspect, re
+    >>> re.findall(r'^\s*(return .*)', inspect.getsource(a_plus_abs_b), re.M)
+    ['return f(a, b)']
+    """
+    if b < 0:
+        f = lambda a,b: a+(-b)
+    else:
+        f = lambda a,b: a+b
+    return f(a, b)
+
+
+def two_of_three(x, y, z):
+    """Return a*a + b*b, where a and b are the two smallest members of the
+    positive numbers x, y, and z.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    >>> # check that your code consists of nothing but an expression (this docstring)
+    >>> # a return statement
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
+    ['Expr', 'Return']
+    """
+    return min(add(square(x),square(y)),add(square(x),square(z)),add(square(y),square(z)))
+
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    fac=1
+    while fac<n:
+        if n%fac==0:
+            largest=fac
+            fac+=1
+        else:
+            fac+=1
+    return largest
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    a=1
+    print(n)
+    while n!=1:
+        if n%2==0:
+            n=n//2
+            print(n)
+            a+=1
+        else:
+            n=n*3+1
+            print(n)
+            a+=1
+    return a
+        
+
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+    n -- a positive integer
+    term -- a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    first=1
+    final=1
+    while first<=n:
+            final= term(first)*final
+            first+=1
+    return final
+        
+        
+
+
+
+def square(x):
+    return x * x
+
+
+def accumulate(merger, base, n, term):
+    """Return the result of merging the first n terms in a sequence and base.
+    The terms to be merged are term(1), term(2), ..., term(n). merger is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    >>> accumulate(lambda x, y: 2 * (x + y), 2, 3, square)
+    58
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
+    """
+    iter=0
+    y=1
+    while iter<=n:
+        if n==0:
+            accum=base
+            iter+=1
+        elif y>0:
+            iter=1
+            accum=merger(base,term(iter))
+            iter+=1
+            y=-1
+        else:
+            accum_1=merger(accum,term(iter))
+            accum=accum_1
+            iter+=1
+    return accum
+
+
+def summation_using_accumulate(n, term):
+    """Returns the sum of term(1) + ... + term(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    return accumulate(add, 0, n, term)
+
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    return accumulate(mul,1,n,term)
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 'equal', 'not equal')
+    'not equal'
+    >>> if_function(3>2, 'bigger', 'smaller')
+    'bigger'
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    61A
+    >>> print(result)
+    None
+    """
+    if cond():
+        return true_func()
+    else:
+        return false_func()
+
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    Welcome to
+    61A
+    >>> print(result)
+    None
+    """
+    return if_function(cond(), true_func(), false_func())
+
+
+def cond():
+    "*** YOUR CODE HERE ***"
+
+
+def true_func():
+    "*** YOUR CODE HERE ***"
+
+
+def false_func():
+    "*** YOUR CODE HERE ***"
